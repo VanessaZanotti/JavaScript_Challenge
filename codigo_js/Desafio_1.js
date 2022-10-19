ab8ed35ff04beac45bf12ae5b93d394eeba759ca
Vanessa Zanotti <81310046+VanessaZanotti@users.noreply.github.com>
2022-10-19 19:52:16 -0300

    Create Desafio_1

---
+++ codigo_js/Desafio_1.js
@@ -0,0 +1,24 @@
+// 1) Observe o trecho de código abaixo:
+// int INDICE = 13, SOMA = 0, K = 0;
+// enquanto K < INDICE faça
+// {
+// K = K + 1;
+// SOMA = SOMA + K;
+// }
+// imprimir(SOMA);
+// Ao final do processamento, qual será o valor da variável SOMA?
+
+function verifySum() {
+  const indice = 13
+  let soma = 0,
+    k = 0
+
+  while (k < indice) {
+    k += 1
+    soma += k
+  }
+
+  console.log('O valor de soma é ' + soma)
+}
+
+verifySum()
